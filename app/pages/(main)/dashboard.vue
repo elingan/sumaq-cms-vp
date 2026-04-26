f1e155dde68b72881ba67bff088c59d8049d5a9d
Eduardo Lingán <elingan@gmail.com>
2026-04-26 09:58:47 +0200

    skills, estilos, etc

--- app/pages/(main)/dashboard.vue
+++ app/pages/(main)/dashboard.vue
@@ -11,24 +11,43 @@
     /> -->
 
     <div class="space-y-8 mt-8 mb-16">
-      <UCard
-        :title="$t('nav.sites')"
-        :description="$t('dashboard.sitesSubtitle')"
-        variant="soft"
-        class="w-full"
-      >
+      <UCard :title="$t('nav.sites')" :description="$t('dashboard.sitesSubtitle')" class="w-full">
         <UPageGrid>
-          <SitesSiteCard
+          <!-- <SitesSiteCard
             v-for="site in sites"
             :key="site.id"
             :site="site"
             :orientation="orientation"
             @refresh="refresh"
+          /> -->
+          <UPageCard
+            v-for="site in siteCards"
+            :key="site.id"
+            :title="site.title"
+            :description="site.description"
+            :image="site.imageUrl"
           />
         </UPageGrid>
+        <UEmpty
+          v-if="!sites?.length"
+          title="No sites"
+          description="No sites found. Please contact your administrator."
+          variant="soft"
+        />
       </UCard>
-      <UCard title="calendar" variant="soft" class="w-full"> content here </UCard>
-      <UPageHeader :title="$t('nav.sites')" :description="$t('dashboard.sitesSubtitle')">
+      <UCard
+        :title="$t('nav.calendar')"
+        :description="$t('dashboard.calendarSubtitle')"
+        class="w-full"
+      >
+        <UEmpty
+          v-if="!calendars?.length"
+          title="No Calendar"
+          description="No calendar found. Please contact your administrator."
+          variant="soft"
+        />
+      </UCard>
+      <!-- <UPageHeader :title="$t('nav.sites')" :description="$t('dashboard.sitesSubtitle')">
         <template #links>
           <UButton
             v-if="canCreateSites"
@@ -54,13 +73,12 @@
         title="Trusted and supported by our amazing community"
         description="We've built a strong, lasting partnership. Their trust is our driving force, propelling us towards shared success."
         :links="siteLinks"
-      />
-      <div v-if="pending" class="flex justify-center">
+      /> -->
+      <div v-if="pendingSites" class="flex justify-center">
         <UIcon name="i-lucide-loader-circle" class="animate-spin size-8 text-muted" />
       </div>
     </div>
-    <!-- </UPageBody> -->
-    <div class="space-y-8 mb-16">
+    <!-- <div class="space-y-8 mb-16">
       <UPageHeader :title="$t('nav.calendar')" :description="$t('dashboard.calendarSubtitle')">
         <template #links>
           <UButton
@@ -76,7 +94,7 @@
         description="We've built a strong, lasting partnership. Their trust is our driving force, propelling us towards shared success."
         :links="siteLinks"
       />
-    </div>
+    </div> -->
 
     <SitesSiteForm v-model:open="showCreateModal" @saved="handleSiteSaved" />
   </UPage>
@@ -90,7 +108,18 @@ definePageMeta({ layout: 'default', title: 'Dashboard' })
 const { canCreateSites } = useRole()
 const showCreateModal = ref(false)
 
-const { data: sites, refresh, pending } = await useFetch('/api/sites')
+const { data: sites, refresh, pending: pendingSites } = await useFetch('/api/sites')
+
+const siteCards = computed(() => {
+  return sites.value?.map((site) => {
+    return {
+      id: site.id,
+      title: site.name,
+      description: site.url,
+      imageUrl: site.thumbnail,
+    }
+  })
+})
 
 const orientation = computed(() =>
   sites.value && sites.value.length > 1 ? 'vertical' : 'horizontal',
